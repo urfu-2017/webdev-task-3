a741d2921e2098f407c9869d805ee3e8de7c1a1e
ftlka <smart.Julia@yandex.ru>
2018-03-20 15:41:27 +0500

    Deleted some comments and extra params from model constructor

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const partialsDir = path.join(viewsDir, 'partials');
 app.set('view engine', 'hbs');
 app.set('views', viewsDir);
 
-// статик надо на что-то поменять, а надо ли?
 app.use(express.static(publicDir));
 
 app.get('/', controller);
--- models/locationModel.js
+++ models/locationModel.js
@@ -1,9 +1,7 @@
 'use strict';
 
-// const queryString = require('query-string');
 const fetch = require('node-fetch');
 const baseUrl = 'https://webdev-task-2-gguhuxcnsd.now.sh';
-// var URLSearchParams = require('url-search-params');
 
 const getApiResp = async (url, options) => {
     const response = await fetch(url, options);
@@ -15,14 +13,6 @@ const getApiResp = async (url, options) => {
 class LocModel {
     constructor(params) {
         this.place = params.place;
-        this.sortBy = params.sortBy;
-        this.findByDescr = params.findByDescr;
-        this.amount = params.amount;
-        this.page = params.page;
-        this.param = params.param;
-        this.value = params.value;
-        this.place1 = params.place1;
-        this.place2 = params.place2;
     }
 
     async get() {
