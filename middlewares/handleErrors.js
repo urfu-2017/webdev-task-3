8a4ac088e4d9b254d1e90103c916d389e3e0ac9b
Paul Khomich <paulkhomich@outlook.com>
2018-03-29 17:14:45 +0500

    Удалил hbs

--- middlewares/handleErrors.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-
-module.exports = (err, req, res, next) => {
-    /* eslint no-unused-vars: 0 */
-    console.error(err);
-    res.render('error');
-};
--- routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { main } = require('./controllers/main');
-
-module.exports = app => {
-    app.get('/', main);
-};
