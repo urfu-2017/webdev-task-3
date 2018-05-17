b533dd0088fec5ec72aded80c2fea48e313d3153
fng3r <dinvk@mail.ru>
2018-05-17 18:21:04 +0300

    constants -> config

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 const path = require('path');
 const express = require('express');
+const { PORT, PUBLIC_DIR } = require('./config');
 const app = express();
-const port = 8080;
-const publicDir = path.join(__dirname, 'public');
 
-app.get('/', (req, res) => res.sendFile(path.join(publicDir, 'index.html')));
-app.use(express.static(publicDir));
-app.listen(port, () => console.info(`Server listening on port ${port}.`));
+app.get('/', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'index.html')));
+app.use(express.static(PUBLIC_DIR));
+app.listen(PORT, () => console.info(`Server listening on port ${PORT}.`));
